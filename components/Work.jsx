b203eb50530fb8708b198c58508cd83bcd742d3a
Ankit Kumar <ankitkr2731@gmail.com>
2025-02-03 02:04:19 +0530

    Enhance Work component layout with improved styling and added Show More button; integrate image assets for better visuals

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,4 +1,5 @@
-import { workData } from "@/assets/assets"
+import { assets, workData } from '@/assets/assets'
+import Image from 'next/image'
 
 const Work = () => {
     return (
@@ -11,13 +12,42 @@ const Work = () => {
                 have done an internship at a startup where I worked on a project
                 from scratch and developed a full-fledged application.
             </p>
-            <div>
-                {workData.map(({bgImage, description, title}) => (
-                    <div key={title} className='' style={{backgroundImage: `url(${bgImage})`}}>
-
+            <div className="grid grid-cols-auto my-10 gap-5">
+                {workData.map(({ bgImage, description, title }) => (
+                    <div
+                        key={title}
+                        className="aspect-square bg-no-repeat bg-center rounded-lg relative cursor-pointer group"
+                        style={{ backgroundImage: `url(${bgImage})` }}
+                    >
+                        <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+                            <div>
+                                <h2 className="font-semibold">{title}</h2>
+                                <p className="text-sm text-gray-700">
+                                    {description}
+                                </p>
+                            </div>
+                            <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+                                <Image
+                                    src={assets.send_icon}
+                                    alt={'send icon'}
+                                    className="w-5"
+                                />
+                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
+            <a
+                href=""
+                className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full px-10 py-3 mx-auto my-20 hover:bg-lightHover duration-500"
+            >
+                Show More{' '}
+                <Image
+                    src={assets.right_arrow_bold}
+                    alt="right arrow"
+                    className="w-4"
+                />
+            </a>
         </div>
     )
 }
