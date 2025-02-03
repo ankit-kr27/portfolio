efc4afa871cd9f5e6ff7a85e7bf24998c2c06e0e
Ankit Kumar <ankitkr2731@gmail.com>
2025-02-03 16:14:51 +0530

    Enhance Footer component with improved layout, contact information, and social media links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,51 @@
-import { assets } from "@/assets/assets"
-import Image from "next/image"
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
 
 const Footer = () => {
-  return (
-    <div>
-      <div>
-        <Image src={assets.logo} alt="" className="w-full" />
-      </div>
-    </div>
-  )
+    return (
+        <div className="mt-20">
+            <div className="text-center">
+                <Image
+                    src={assets.logo}
+                    alt="logo"
+                    className="w-36 mx-auto mb-2"
+                />
+            </div>
+            <div className="w-max flex items-center gap-2 mx-auto">
+                <Image src={assets.mail_icon} alt="mail icon" className="w-6" />
+                ankitkr2731@gmail.com
+            </div>
+            <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
+                <p>&copy; 2025 Ankit Kumar. All rights reserved.</p>
+                <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
+                    <li>
+                        <a
+                            href="https://github.com/ankit-kr27/"
+                            target="_blank"
+                        >
+                            GitHub
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://www.linkedin.com/in/ankit-kr27"
+                            target="_blank"
+                        >
+                            LinkedIn
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://x.com/ank_it_kr"
+                            target="_blank"
+                        >
+                            Twitter
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
